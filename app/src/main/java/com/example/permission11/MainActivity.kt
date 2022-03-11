bcf3b2815e9eaf8f81590c75ef5830a694926ba8
ArkaMondalAppstango <arka.m@appstangodev.com>
2022-03-11 11:39:27 +0530

    Add live Data to Checking Internet Connection

--- app/src/main/java/com/example/permission11/MainActivity.kt
+++ app/src/main/java/com/example/permission11/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.example.permission11
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import com.example.permission11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityBinding : ActivityMainBinding
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     private var isReadPermissionGranted = false
     private var isLocationPermissionGranted = false
     private var isRecordPermissionGranted = false
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
 
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
 
@@ -27,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         requestPermission()
+
+        val networkConnection = NetworkConnectionHelper(applicationContext)
+        networkConnection.observe(this, Observer {
+            if (it){
+                activityBinding.textV.text = "Connected"
+            }else{
+                activityBinding.textV.text = "Disconnected"
+            }
+        })
+
+        //activityBinding.textV.text = isOnline(this).toString()
     }
 
     private fun requestPermission(){
@@ -70,5 +92,28 @@ class MainActivity : AppCompatActivity() {
             permissionLauncher.launch(permissionRequest.toTypedArray())
         }
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
